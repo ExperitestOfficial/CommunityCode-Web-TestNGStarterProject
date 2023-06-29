aa8bec452a59ba150768af2b5d0778f2815dbf3f
Noa Tel-or <127393634+ntelorDAI@users.noreply.github.com>
2023-06-29 13:48:15 +0300

    SA-49098  Web - Starter Project - JavaTestNG

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# CommunityCode-Web-TestNGStarterProject
-Starter project with Java TestNG
+# CommunityCode Web: TestNG Starter Project
+The TestNG Starter Project enables you to quickly run tests with minimal coding. The project itself compiles and can be run immediately.
+## Setting Up the Project
+
+To set up the project:
+1. Clone the project by running the following:
+   ```
+   git clone https://github.com/ExperitestOfficial/CommunityCode-Web-TestNGStarterProject
+   cd CommunityCode-Web-TestNGStarterProject
+   ```   
+1. Set up authentication by updating the following parameters in [cloud.properties](cloud.properties):
+    * cloud.url - Url for the cloud the test would run in. For example, https://company.experitest.com/
+    * cloud.accessKey -  Personal authentication. See [Obtaining Access Key](https://docs.experitest.com/pages/viewpage.action?pageId=52593435) to learn how to obtain an access key.
+
+## Running Tests
+
+To run the tests in this project, run the following on the command line:
+
+```bash
+./gradlew test
+```
+
+## Desired Capabilities
+In this example we use desired capabilities to run a test on chrome. Change the browser name in the capabilities to run you test on different browsers.
+
+```
+dc.setCapability(CapabilityType.BROWSER_NAME, "chrome");
+```
+
+Continuous Cloud Testing expands Selenium's capabilities and allows better control over your test.
+
+See [Capabilities In Selenium Tests](https://docs.experitest.com/display/TE/Capabilities+In+Selenium+Tests) to learn how to customize the desired capabilities for your tests.
+
+## Documentation
+To find out more about Continuous Cloud Testing usage, features, and best practices, visit our online [documentation](https://docs.experitest.com/display/TE/Test+Execution+Home).
+
+## Support
+If you encounter an issue that is not covered here or in our online documentation, contact us at [support@digital.ai](mailto:support@digital.ai).
---
+++ src/test/java/WebExampleTest.java
@@ -0,0 +1,55 @@
+import java.time.Duration;
+import org.openqa.selenium.*;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.URL;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+
+public class WebExampleTest extends BaseTest {
+	
+	private RemoteWebDriver driver;
+	
+	@BeforeTest
+	public void setUp() throws Exception {
+		dc.setCapability("experitest:testName", "Web Example Test");
+		dc.setCapability("experitest:accessKey", getAccessKey());
+		
+		/* Change this to run your test on different browsers. */
+		dc.setCapability(CapabilityType.BROWSER_NAME, "MicrosoftEdge");
+		
+		driver = new RemoteWebDriver(new URL(getCloudUrl(), "/wd/hub"), dc);
+	}
+	
+	@Test
+	public void demoBankTest() {
+		driver.get("https://demo-bank.ct.digital.ai");
+		
+		new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.presenceOfElementLocated(By.id("login")));
+		
+		driver.findElement(By.xpath("//*[@data-auto='username']//input")).sendKeys("company");
+		driver.findElement(By.xpath("//*[@data-auto='password']//input")).sendKeys("company");
+		driver.findElement(By.xpath("//*[@data-auto='login']")).click();
+		
+		driver.findElement(By.xpath("//*[@data-auto='transfer-funds']")).click();
+		
+		driver.findElement(By.xpath("//input[@name='NAME']")).sendKeys("John");
+		driver.findElement(By.xpath("//input[@name='PHONE']")).sendKeys("1-234-5678");
+		driver.findElement(By.xpath("//input[@name='AMOUNT']")).sendKeys("1000");
+		driver.findElement(By.xpath("//input[@name='COUNTRY']/..")).click();
+		driver.findElement(By.xpath("//*[text()='INDIA']")).click();
+		driver.findElement(By.xpath("//*[@data-auto='transfer-button']")).click();
+	}
+	
+	@AfterTest
+	public void tearDown() {
+		System.out.println("Report URL: "+ driver.getCapabilities().getCapability("reportUrl"));
+		driver.quit();
+	}
+	
+}
